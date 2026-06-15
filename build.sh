55cd42587930916dd28f4cdbebc54d3eed4314ec
SÁGODI Zoltán <sagodiz@inf.u-szeged.hu>
2026-06-15 13:41:44 +0200

    Add build script for SM

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#cd log4cplus-1.1.0
+
+#make distclean
+
+#export ANALYZER_DISABLE_ANALYSIS=true
+#./configure CXX=clang++  CC=clang
+#unset ANALYZER_DISABLE_ANALYSIS
+
+#make
+
+clang++ -fno-stack-protector -no-pie -O0 \
+    -I/usr/include/postgresql /challenge_buffer_overflow/vuln_app.cpp \
+    -o vuln_app \
+    $(pkg-config --cflags --libs opencv4) -lpq
